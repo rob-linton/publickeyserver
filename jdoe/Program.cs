dcca72b926c4ab15932630eb9b6f785051825408
Rob <rob.linton@mac.com>
2023-11-21 16:12:32 +1100

    jdoe command line basics

---
+++ jdoe/Program.cs
@@ -0,0 +1,30 @@
+﻿using CommandLine;
+namespace jdoe;
+
+public class Options
+{
+    [Option('r', "read", Required = true, HelpText = "Input files to be processed.")]
+    public IEnumerable<string>? InputFiles { get; set; }
+
+	[Option('v', "verbose", Default = false, HelpText = "Set output to verbose messages.")]
+	public bool Verbose { get; set; } = false;
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+		Console.WriteLine("Hello World!");
+
+        Parser.Default.ParseArguments<Options>(args)
+           .WithParsed<Options>(o =>
+           {
+               if (o.Verbose!)
+               {
+                   Console.WriteLine($"Verbose mode is enabled. Verbose level: {o.Verbose}");
+               }
+
+               Console.WriteLine($"Read files: {string.Join(",", o.InputFiles!)}");
+           });
+    }
+}
