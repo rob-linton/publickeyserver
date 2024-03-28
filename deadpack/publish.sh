f252a67e83311d0dad68a5ae4a14817fb46449fb
Rob <rob.linton@mac.com>
2024-03-28 14:27:21 +1100

    publish and webpage updates

--- deadpack/publish.sh
+++ deadpack/publish.sh
@@ -1,1 +1,7 @@
 dotnet publish -c Release -p:PublishSingleFile=true -r osx-x64 --self-contained -p:IncludeNativeLibrariesForSelfExtract=true
+dotnet publish -c Release -p:PublishSingleFile=true -r win-x64 --self-contained -p:IncludeNativeLibrariesForSelfExtract=true
+dotnet publish -c Release -p:PublishSingleFile=true -r linux-x64 --self-contained -p:IncludeNativeLibrariesForSelfExtract=true
+
+cp /Users/rob/Documents/src/publickeyserver/deadpack/bin/Release/net8.0/linux-x64/publish/deadpack ./publish/linux-x64/deadpack
+cp /Users/rob/Documents/src/publickeyserver/deadpack/bin/Release/net8.0/osx-x64/publish/deadpack ./publish/osx-x64/deadpack
+cp /Users/rob/Documents/src/publickeyserver/deadpack/bin/Release/net8.0/win-x64/publish/deadpack.exe ./publish/win-x64/deadpack.exe
