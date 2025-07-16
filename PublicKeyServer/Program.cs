e2e97a8f204f456411d4b1ecf17da94d90a43f96
rob-linton <rob.linton@mac.com>
2025-07-16 22:09:34 +1000

    port 80

--- PublicKeyServer/Program.cs
+++ PublicKeyServer/Program.cs
@@ -197,7 +197,7 @@ namespace publickeyserver
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                    webBuilder.UseUrls("http://0.0.0.0:5001/");
+                    webBuilder.UseUrls("http://0.0.0.0:80/");
                 });
     }
 }
