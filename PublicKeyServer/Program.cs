61a6073f7c4c472cfe6e7a8c157b59a7f73a43b7
rob-linton <rob.linton@mac.com>
2025-07-16 22:20:17 +1000

    port 5000

--- PublicKeyServer/Program.cs
+++ PublicKeyServer/Program.cs
@@ -197,7 +197,7 @@ namespace publickeyserver
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                    webBuilder.UseUrls("http://0.0.0.0:80/");
+                    webBuilder.UseUrls("http://0.0.0.0:5000/");
                 });
     }
 }
